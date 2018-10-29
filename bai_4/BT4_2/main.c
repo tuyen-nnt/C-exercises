5d9085a25b5625896e890826d889e5bc91006dcf
ThanhTuyen <contact@tuyen.me>
2018-10-30 01:05:35 +0700

    Updating BT 4,2

--- bai_4/BT4_2/main.c
+++ bai_4/BT4_2/main.c
@@ -1,20 +1,35 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
-//chua link def cua libm.a vao thu vien math.h nen ko chay dc
+
+// chua link def cua libm.a vao thu vien math.h nen ko chay dc
 int main() {
-    float a,c;
-    double b,x1,x2,D;
-    printf("Nhap vao a, b, c");
-    scanf("%f %lf %f", &a, &b, &c);
-    D = pow(b,2.0) - 4.0*a*c;
+    float a, c, b, x1, x2, D;
+
+    char buf1[10];
+    char buf2[10];
+    char buf3[10];
+
+    printf("Nhap vao a, b, c: ");
+    scanf("%s %s %s", buf1, buf2, buf3);
+
+    a = strtof(buf1, NULL);
+    b = strtof(buf2, NULL);
+    c = strtof(buf3, NULL);
+
+    D = b*b - 4*a*c;
+
     if (D < 0)
+    {
         printf ("Phuong trinh vo nghiem.");
+    }
     else
     {
         x1 = (-b - sqrt(D))/(2.0*a);
         x2 = -b/a - x1;
-        printf("x1 = %4.1lf\n x2 = %4.1lf",x1,x2);
+        printf("x1 = %4.1f\n x2 = %4.1f", x1, x2);
     }
+
     return 0;
 }
