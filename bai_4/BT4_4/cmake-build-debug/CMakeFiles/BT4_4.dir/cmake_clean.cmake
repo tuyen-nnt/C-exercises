509f647eadded29722a097bc132ecf4eea0ea66b
ThanhTuyen <contact@tuyen.me>
2018-10-30 15:09:09 +0700

    up BT 4_4 mrhuy

---
+++ bai_4/BT4_4/cmake-build-debug/CMakeFiles/BT4_4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BT4_4.dir/main.c.o"
+  "BT4_4.pdb"
+  "BT4_4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BT4_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
