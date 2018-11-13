faffb143fef4bd9f49b9b5ab9e6f2e33c73d9bea
ThanhTuyen <contact@tuyen.me>
2018-11-13 12:23:39 +0700

    Still on working

---
+++ bai_7/BT7_2/cmake-build-debug/CMakeFiles/BT7_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BT7_2.dir/main.c.o"
+  "BT7_2.pdb"
+  "BT7_2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BT7_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- bai_7/BT7_2/main.c
+++ bai_7/BT7_2/main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+void nhapMang(int** array, int* size){
+    *size = 0;
+    char buffer[10];
+
+    while (*size <= 0) {
+        printf("Nhap vao so luong phan tu: ");
+        scanf("%s", buffer);
+        *size = (int) strtol(buffer, NULL, 10);
+    }
+    *array = (int*) calloc(*size, sizeof(int));
+    for (int i = 0; i < *size, i++){
+        printf("Nhap vao phan tu thu %d", i+1);
+        scanf("%s", buffer);
+        (*array)[i] = (int)strtol(buffer, NULL, 10);
+    }
+}
 int main() {
-    printf("Hello, World!\n");
+    int size1 = 0;
+    int* a = NULL;
+    nhapMang(&a, &size1);
+    for (int i = 0; i < size1; i++)
+        printf("%d", a[i]);
+
+
     return 0;
 }
