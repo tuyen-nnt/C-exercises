36ce893ef6b77489416b0cf19fdd3fd61638a3ea
ThanhTuyen <contact@tuyen.me>
2018-11-15 09:28:10 +0700

    fixed all problems

--- bai_7/BT7_3/main.c
+++ bai_7/BT7_3/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <stdlib.h>
+
 #define IN 1
 #define OUT 0
 
@@ -31,33 +32,56 @@ return check;
 }
 
 void mangPalindrome(int** array, int** palindrome, int* size){//tham so dien vao la mang a
-    *palindrome = (int*)calloc(*size,sizeof(int));
+    *palindrome = (int*)calloc(*size, sizeof(int));
     //(*palindrome)[0] = (*array)[*size - 1];
     //(*palindrome)[*size - 1] = (*array)[0];
     for (int i = 0; i < *size; i++)
         (*palindrome)[i] = (*array)[(*size-1) - i];
 }
 
-int ktPalindrome(int** array, int** palindrome, int* size){
+int ktPalindrome(int* array, int* palindrome, int size){
     int check = IN;
     int i = 0;
-    while ((check == IN)&&(i < *size)) {
-            if ((*array)[i] != (*palindrome)[i]){
-                check = OUT;
-                i++; }
+
+    while ((check == IN) && (i++ < size))
+    {
+        if (array[i] != palindrome[i])
+        {
+            return OUT;
+        }
     }
+
     return check;
 }
 
-int ktCSC(int** array, int* size, int* psai){
+int ktCSC(int* array, int size)
+{
+    if (size <= 1)
+        return IN;
+
+    int psai = array[1] - array[0];
+
     int check = IN;
     int i = 1;
-    while ((check == IN)&&(i < *size)) {
-        if ((*array)[i+1] != ((*array)[i] + *psai))
-                check = OUT;
+    while ((check == IN) && (i < size-1))
+    {
+        if (array[i+1] != (array[i] + psai))
+            check = OUT;
+
         i++;
     }
+
     return check;
+
+    /*for (int i = 2; i < size; i++)
+    {
+        if (array[i] - array[i-1] != psai)
+        {
+            return OUT;
+        }
+    }
+
+    return IN;*/
 }
 
 int main() {
@@ -76,14 +100,15 @@ int main() {
     for (int i = 0 ; i < size1; i++)
        printf("%d\n", p[i]);
 
-    if (ktPalindrome(&a, &p, &size1) == OUT)
+    if (ktPalindrome(a, p, size1) == OUT)
         printf("Mang khong doi xung.\n");
-    else printf("Mang doi xung.\n");
+    else
+        printf("Mang doi xung.\n");
 
-    int d = a[1] - a[0];
-    if (ktCSC(&a, &size1, &d) == IN)
+    if (ktCSC(a, size1) == IN)
         printf("Mang la cap so cong.");
-    else printf ("Mang khong la cap so cong.");
+    else
+        printf ("Mang khong la cap so cong.");
 
     return 0;
 }
