f2aac5c7aad8840a97b525168ec3bb0403a7b043
ThanhTuyen <contact@tuyen.me>
2018-11-15 23:33:00 +0700

    pull comments, complimentary adding & learn about string declaration

--- bai_7/BT7_4/main.c
+++ bai_7/BT7_4/main.c
@@ -1,47 +1,85 @@
-#include <stdio.h>
-#include <memory.h>
-#include <malloc.h>
+#include <stdio.h>      // Sử dụng các hàm printf(), scanf()
+#include <stdlib.h>     // Sử dụng malloc()
+#include <string.h>     // Sử dụng strlen(), strncpy
 
 // Copy n kí tự từ s2 và chèn vào sau s1
 void stringCopy(char* s1, char* s2, int n)
 {
-    int pLastChar = (int) (strlen(s1) - 1);
+    // Ngắt nếu input không hợp lệ
+    if (s1 == NULL || s2 == NULL || n == 0)
+    {
+        return;
+    }
 
+    // Chỉ số của null terminated character trong s1
+    // (là vị trí sẽ chèn s2 vào)
+    int pNextChar = strlen(s1);     // s[pNextChar] = '\0'
+
+    // Copy s2 vào s1
     for (int i = 0; i < n; i++)
     {
-        s1[pLastChar+1+i] = s2[i];
+        s1[pNextChar++] = s2[i];
     }
+
+    s1[pNextChar] = '\0';
 }
 
-void insert(char* s1, char* s2, char* s)
+// Chèn s1 vào giữa s2
+// Có thể sử dụng hàm mặc định của c là strncpy() thay cho stringCopy tự viết
+// https://www.tutorialspoint.com/c_standard_library/c_function_strncpy.htm
+char* insert(char* s1, char* s2)
 {
+    // Ngắt nếu input không hợp lệ
+    if (s1 == NULL || s2 == NULL)
+    {
+        return NULL;
+    }
 
-    /*for (int i = 0; i < (strlen(s2)/2); i++)
-        s[i]= s2[i];
-    for (int i = 0; (i + strlen(s2)/2) < (strlen(s1)); i++)
-        s[i + strlen(s2)/2] = s1[i];
-    for (int i = 0; i < (strlen(s1)/2); i++)
-        s[i + (strlen(s2)/2) + strlen(s1)] = s2[strlen(s2)+1];*/
+    int sizeS1 = strlen(s1);
+    int sizeS2 = strlen(s2);
 
-    int sizeS2 = (int) strlen(s2);
-    int mid = sizeS2 / 2;
+    // Cấp phát bộ nhớ để lưu chuỗi s
+    char* s = (char*)malloc(sizeS1 + sizeS2 + 1);
+    //char* s = (char*)calloc(sizeS1 + sizeS2 + 1, sizeof(char));
 
-    stringCopy(s, s2 + mid, sizeS2 - mid);
+    // Chỉ số của kí tự nằm giữa xâu s2 mà tại đó ta sẽ cắt s2
+    int pMidS2 = (sizeS2 + 1) / 2;
 
+    // Copy nửa đầu của s2 vào s
+    stringCopy(s, s2, pMidS2);
+    //strncpy(s, s2, pMidS2);
+
+    // Copy s1 vào s
+    stringCopy(s, s1, sizeS1);
+    //strncpy(s + pMidS2, s1, sizeS1);
+
+    // Copy nửa sau của s2 vào s
+    stringCopy(s, s2 + pMidS2, sizeS2 - pMidS2);
+    //strncpy(s + pMidS2 + sizeS1, s2 + pMidS2, sizeS2 - pMidS2);
+
+    // %lu là format của kiểu unsigned long
+    printf("strlen(s) = %lu\n", strlen(s));
+
+    return s;
 }
 
 int main() {
-    char s1[10] = {};
-    char s2[10] = {};
-    char s[20] = {};
+    char s1[100] = "";
+    char s2[100] = "";
 
-    printf("Nhap vao xau s1 : ");
+    printf("Nhap vao xau s1: ");
     scanf("%s", s1);
-    printf("Nhap vao xau s2 : ");
+    printf("Nhap vao xau s2: ");
     scanf("%s", s2);
 
-    insert(s1, s2, s);
-    for (int i = 0; i < (strlen(s1) + strlen(s2)); i++)
-        printf("%s", s);
+    // Chèn chuỗi s1 vào giữa s2
+    char* s = insert(s1, s2);
+
+    printf("s = %s\n", s);
+
+    // Giải phóng vùng nhớ của s
+    // vì con trỏ s được khởi tạo bởi malloc() / calloc()
+    free(s);
+
     return 0;
-}0
+}
