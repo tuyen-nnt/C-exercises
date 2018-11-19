b5793ca819c61f5846ffcbbf731bddb311a2a511
ThanhTuyen <contact@tuyen.me>
2018-11-19 18:13:43 +0700

    still on working

---
+++ bai_8/BT8_1/cmake-build-debug/CMakeFiles/BT8_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BT8_1.dir/main.c.o"
+  "BT8_1.pdb"
+  "BT8_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BT8_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
