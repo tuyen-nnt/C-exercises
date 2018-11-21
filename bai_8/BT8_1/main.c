66f15c55d110348ebc3ed311fa5943bc911349f2
ThanhTuyen <contact@tuyen.me>
2018-11-21 11:59:34 +0700

    update min-max sum-of-line solution

--- bai_8/BT8_1/main.c
+++ bai_8/BT8_1/main.c
@@ -1,31 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void nhapMang(int a[10][10], int* size)
-{
-    int i,j;
+void nhapMang(int a[10][10], int* size) {
+    int i, j;
     char buffer[10] = {};
     *size = 0;
-    while (*size <= 0 )
-    {
+    while (*size <= 0) {
         printf("Nhap vao so hang va so cot N = ");
         scanf("%s", buffer);
         *size = (int) strtol(buffer, NULL, 10);
     }
 
-    for (i = 0; i < *size; i++)
-    {
-        for (j = 0; j < *size; j++)
-        {
+    for (i = 0; i < *size; i++) {
+        for (j = 0; j < *size; j++) {
             printf("Nhap vao a[%d][%d] = ", i, j);
             scanf("%s", buffer);
             a[i][j] = (int) strtol(buffer, NULL, 10);
         }
     }
 
-
+}
     //Dong co tong lon nhat:
-
+int lineMAX(int array[10][10], int size)
+{
     int sMAX = 0;
     int s = 0;
     int remember = 0;
@@ -34,17 +31,45 @@ void nhapMang(int a[10][10], int* size)
     {
         for (int j = 0; j < size; j++)
         {
-            s += a[i][j];
+            s += array[i][j];
+            if (s > sMAX)
+            {
+                sMAX = s;
+                remember = i;
+            }
         }
-        if (s > sMAX)
-            sMAX = s;
-        remember = i; 
+        s = 0;
     }
-
-    printf("Dong co tong lon nhat la dong thu %d.", remember + 1);
-
+    return remember + 1;
 }
 
+int lineMIN(int array[10][10], int size)
+{
+    int sMIN = 0;
+    int s = 0;
+    int remember = 0;
+
+    for (int j = 0; j < size; j++)
+    {
+        sMIN += array[0][j];
+    }
+
+    for (int i = 1; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            s += array[i][j];
+            if (s < sMIN)
+            {
+                sMIN = s;
+                remember = i;
+            }
+        }
+        s = 0;
+    }
+
+    return remember + 1;
+}
 
 int main() {
 
@@ -73,7 +98,7 @@ int main() {
                 s1 += a[i][i];
         }
     }
-    printf("Tong duong cheo chinh = %d", s1);
+    printf("Tong duong cheo chinh = %d\n", s1);
 
     //Tinh tong duong cheo phu:
 
@@ -87,8 +112,10 @@ int main() {
                 s2 += a[i][j];
         }
     }
+    printf("Tong duong cheo phu = %d\n", s2);
 
-
+    printf("Dong co tong lon nhat la dong thu %d.\n",lineMAX(a, size));
+    printf("Dong co tong nho nhat la dong thu %d.\n",lineMIN(a, size));
 
     return 0;
 }
